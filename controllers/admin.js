5853d702f03d3e75ae13b469abd0b90230044987
PMehar <pujamehar05@gmail.com>
2023-06-08 14:09:34 +0530

    did crud operation using sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,10 +13,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-  .then(()=>{
-    res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+  .then(result=>{
+    //console.log(result);
+    console.log('CREATED PRODUCT');
+    res.redirect('/admin/products');
   })
   .catch(err=>{
     console.log(err);
@@ -30,8 +36,8 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product])=>{
+  Product.findByPk(prodId)
+  .then(product=>{
     if (!product) {
       return req.redirect("/");
     }
@@ -54,27 +60,29 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save()
-  .then(()=>{
+  Product.findByPk(prodId)
+  .then(product=>{
+    product.title= updatedTitle;
+    product.price= updatedPrice;
+    product.description= updatedDesc;
+    product.imageUrl=updatedImageUrl;
+    return product.save();
+  })
+  .then(result =>{
+    console.log('UPDATED PRODUCT');
     res.redirect('/admin/products');
   })
   .catch(err=>{
     console.log(err);
   });
+  
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows,fieldData])=>{
+  Product.findAll()
+  .then((products)=>{
     res.render("admin/products", {
-        prods: rows,
+        prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
       });
@@ -86,8 +94,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req,res,next) =>{
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-  .then(()=>{
+  Product.findByPk(prodId)
+  .then(product=>{
+    return product.destroy(); 
+  })
+  .then(result =>{
+    console.log('DESTROYED PRODUCT');
     res.redirect('/admin/products');
   })
   .catch(err=>{
